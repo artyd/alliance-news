ea475ea0d77c8dea2397d4766fd06f61de75057e
artyd <82162354+artyd@users.noreply.github.com>
2026-03-26 22:56:37 +0200

    Add files via upload

---
+++ database.py
@@ -0,0 +1,39 @@
+import os
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+def init_db():
+    conn = psycopg2.connect(SQLALCHEMY_DATABASE_URL)
+    conn.autocommit = True
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS articles (
+            id SERIAL PRIMARY KEY,
+            title TEXT NOT NULL,
+            link TEXT UNIQUE NOT NULL,
+            published TEXT,
+            category TEXT NOT NULL,
+            image_url TEXT,
+            summary_en TEXT,
+            summary_ua TEXT,
+            summary_ru TEXT
+        )
+    ''')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS telegram_users (
+            chat_id BIGINT PRIMARY KEY,
+            language TEXT DEFAULT 'en'
+        )
+    ''')
+    
+    conn.close()
+
+def get_db_connection():
+    conn = psycopg2.connect(SQLALCHEMY_DATABASE_URL)
+    return conn
